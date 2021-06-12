070f3fad3c9fe1faa4ab0b00313983fd435ac578
diogo gomes <diogogomes467@gmail.com>
2021-06-12 16:20:08 -0400

    dia 12/06/21

--- src/test/java/Aplicação_web/Home.java
+++ src/test/java/Aplicação_web/Home.java
@@ -1,4 +1,32 @@
 package Aplicação_web;
 
+import Pages.Home.HomePage;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Home {
-}
+    static WebDriver driver;
+    static HomePage Home;
+    static Object homeXpath;
+
+
+    public void Home(WebDriver driver) {
+        Aplicação_web.Home.driver = driver;
+    }
+
+    @Before
+    public void setHomePage() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://triosdecapacitacao.eadplataforma.com/");
+    }
+
+        @Test
+
+        public void Test () {
+            HomePage h = new HomePage(driver);
+            h.homeXpath(driver);
+        }
+    }
--- src/test/java/Pages/Home/HomePage.java
+++ src/test/java/Pages/Home/HomePage.java
@@ -1,85 +1,137 @@
 package Pages.Home;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class HomePage {
 
-    static WebDriver driver;
+    public WebDriver driver;
 
-    public HomePage(WebDriver driver){
-
+    public HomePage(WebDriver driver) {
         this.driver = driver;
     }
-    public void homeXpath(){
 
-        WebElement iconeTrios = driver.findElement(By.linkText("class=navbar-brand"));
-        iconeTrios.click();
+    public void homeXpath(WebDriver driver) {
+        /*
+        WebElement h1 = driver.findElement(By.partialLinkText("PLANO TESTES FUNCIONAIS WEB E MOBILE"));
+        h1.click();
+        */
+        driver.findElement(By.cssSelector("body")).sendKeys(Keys.CONTROL+"n");
+        try{
+            Thread.sleep(300);
+            for (String winHandle:driver.getWindowHandles()){
+                driver.switchTo().window(winHandle);
+            }
+            driver.get("https://triosdecapacitacao.eadplataforma.com/plano/plano-teste-funcional/");
+            driver.get("https://triosdecapacitacao.eadplataforma.com/plano/planos-testes-automatizados/");
+            driver.get("https://triosdecapacitacao.eadplataforma.com/plano/plano-teste-funcional-api/");
+            driver.get("https://triosdecapacitacao.eadplataforma.com/plano/plano-testes-automatizados-web-mobile/");
+            driver.get("https://triosdecapacitacao.eadplataforma.com/plano/plano-testes-automatizados-mobile-ios/");
+            driver.get("https://triosdecapacitacao.eadplataforma.com/plano/plano-testes-automatizados-api/");
+            driver.get("https://triosdecapacitacao.eadplataforma.com/plano/plano-teste-automatizados-swe/");
+            driver.get("https://triosdecapacitacao.eadplataforma.com/plano/plano-testes-automatizados-api-rest/");
+            driver.get("https://triosdecapacitacao.eadplataforma.com/plano/plano-teste-automatizado-api-postman/");
+        } catch (InterruptedException e) {
 
-        WebElement home  = driver.findElement(By.linkText("HOME"));
-        home.click();
+            e.printStackTrace();
+        }
+//precisa de um click na logo para ser direcionado novamente para home
+    WebElement pesquisa = driver.findElement(By.id("searchtext"));
+    pesquisa.sendKeys("teste funcional web");
 
-        WebElement blog  = driver.findElement(By.linkText("BLOG"));
-        blog.click();
+    WebElement lupa = driver.findElement(By.id("btn_form_search"));
+    lupa.click();
 
-        WebElement sobre  = driver.findElement(By.linkText("SOBRE NÒS"));
-        sobre.click();
+        driver.findElement(By.cssSelector("body")).sendKeys(Keys.CONTROL+"n");
+        try {
+            Thread.sleep(300);
+            for (String winHandle : driver.getWindowHandles()) {
+                driver.switchTo().window(winHandle);
+            }
+            //combo de testes funcionais
+            //saiba mais
+            driver.get("https://triosdecapacitacao.eadplataforma.com/curso/teste-funcional-aplicacao-web-e-mobile-2020-12-15-22-38-04/");
+            //matricule-se
+            driver.get("https://triosdecapacitacao.eadplataforma.com/checkout/course/21/");
+            //saiba mais
+            driver.get("https://triosdecapacitacao.eadplataforma.com/curso/teste-funcional-aplicacao-web-e-mobile-2020-12-15-22-38-04-2020-12-15-23-54-43/");
+            //matricule-se
+            driver.get("https://triosdecapacitacao.eadplataforma.com/checkout/course/22/");
+            //saiba mais
+            driver.get("https://triosdecapacitacao.eadplataforma.com/curso/teste-funcional-aplicacao-web-e-mobile-2020-12-15-22-33-37/");
+            //saiba mais está duplicado porque consta esgotado direcionando pro mesmo lugar
+            driver.get("https://triosdecapacitacao.eadplataforma.com/curso/teste-funcional-aplicacao-web-e-mobile-2020-12-15-22-33-37/");
+            //saiba mais
+            driver.get("https://triosdecapacitacao.eadplataforma.com/curso/teste-funcional-aplicacao-api-2020-12-15-22-29-10/");
+            //matricule-se
+            driver.get("https://triosdecapacitacao.eadplataforma.com/checkout/course/19/");
+            //saiba mais
+            driver.get("https://triosdecapacitacao.eadplataforma.com/curso/teste-funcional-aplicacao-api-2020-12-15-22-29-10-2021-02-07-08-26-35/");
+            //matricule-se
+            driver.get("https://triosdecapacitacao.eadplataforma.com/checkout/course/42/");
+            //saiba mais
+            driver.get("https://triosdecapacitacao.eadplataforma.com/curso/teste-funcional-aplicacao-api-2020-12-15-22-29-10-2021-02-07-08-15-38/");
+            //matricule-se
+            driver.get("https://triosdecapacitacao.eadplataforma.com/checkout/course/41/");
 
-        WebElement treinamento  = driver.findElement(By.linkText("TREINAMENTOS ONLINE"));
-        treinamento.click();
+            //combo de testes automatizados
 
-        WebElement empresa  = driver.findElement(By.linkText("EMPRESA"));
-        empresa.click();
+            //saiba mais
+            driver.get("https://triosdecapacitacao.eadplataforma.com/curso/teste-automatizado-na-aplicacao-web/");
+            //matricule-se
+            driver.get("https://triosdecapacitacao.eadplataforma.com/checkout/course/6/");
+            //saiba mais
+            driver.get("https://triosdecapacitacao.eadplataforma.com/curso/teste-automatizado-na-aplicacao-web-2020-10-11-20-08-28-2020-10-11-20-29-32/");
+            //matricule se
+            driver.get("https://triosdecapacitacao.eadplataforma.com/checkout/course/15/");
+            //saiba mais
+            driver.get("https://triosdecapacitacao.eadplataforma.com/curso/teste-automatizado-na-aplicacao-web-2020-10-11-20-08-28-2020-10-11-20-29-32/");
+            //matricule-se
+            driver.get("https://triosdecapacitacao.eadplataforma.com/checkout/course/15/");
+            //saiba mais
+            driver.get("https://triosdecapacitacao.eadplataforma.com/curso/teste-automatizado-na-aplicacao-web-2020-12-16-00-48-48/");
+            //matricule-se
+            driver.get("https://triosdecapacitacao.eadplataforma.com/checkout/course/23/");
+            //saiba mais
+            driver.get("https://triosdecapacitacao.eadplataforma.com/curso/teste-automatizado-na-aplicacao-web-2020-10-11-20-08-28-2020-10-11-20-29-32-2020/");
+            //matricule-se
+            driver.get("https://triosdecapacitacao.eadplataforma.com/checkout/course/24/");
+            //saiba mais
+            driver.get("https://triosdecapacitacao.eadplataforma.com/curso/teste-automatizado-na-aplicacao-web-2020-10-11-20-08-28-2020-12-16-00-56-04/");
+            //matricule-se
+            driver.get("https://triosdecapacitacao.eadplataforma.com/checkout/course/25/");
+            //saiba mais
+            driver.get("https://triosdecapacitacao.eadplataforma.com/curso/teste-automatizado-na-aplicacao-api-soapui-2020-12-16-00-58-01/");
+            //matricule-se
+            driver.get("https://triosdecapacitacao.eadplataforma.com/checkout/course/26/");
+            //saiba mais
+            driver.get("https://triosdecapacitacao.eadplataforma.com/curso/teste-automatizado-na-aplicacao-api-swagger-2020-12-16-00-59-39/");
+            //matricule-se
+            driver.get("https://triosdecapacitacao.eadplataforma.com/checkout/course/27/");
+            //saiba mais
+            driver.get("https://triosdecapacitacao.eadplataforma.com/curso/teste-automatizado-na-aplicacao-api-rest-assured-2020-12-16-01-02-08/");
+            //matricule-se
+            driver.get("https://triosdecapacitacao.eadplataforma.com/curso/api-postman-2020-12-16-01-03-42/");
+            //matricule-se
+            driver.get("https://triosdecapacitacao.eadplataforma.com/checkout/course/29/");
+            //saiba mais
+            driver.get("https://triosdecapacitacao.eadplataforma.com/curso/api-postman-2020-12-16-01-06-34/");
+            //matricule-se
+            driver.get("https://triosdecapacitacao.eadplataforma.com/checkout/course/30/");
+            //saiba mais
+            driver.get("https://triosdecapacitacao.eadplataforma.com/curso/teste-automatizado-api-karate/");
+            //matricule-se
+            driver.get("https://triosdecapacitacao.eadplataforma.com/checkout/course/17/");
 
-        WebElement contato  = driver.findElement(By.linkText("CONTATO"));
-        contato.click();
-
-        WebElement entrar  = driver.findElement(By.linkText("menu_entrar"));
-        entrar.click();
-
-        WebElement cadastre  = driver.findElement(By.linkText("btn-custom custom1"));
-        cadastre.click();
-
-        WebElement saibamais  = driver.findElement(By.linkText("btn-custom custom2"));
-        saibamais.click();
-
-        WebElement t1  = driver.findElement(
-                        By.linkText("TESTE FUNCIONAL API KARATE (ABRIL/2021) "));
-        t1.click();
-
-        WebElement t2  = driver.findElement(
-                    By.linkText("TESTE FUNCIONAL DE API POSTMAN (ABRIL/2021)"));
-        t2.click();
-
-        WebElement t3  = driver.findElement(
-                       By.linkText("TESTE FUNCIONAL DE API REST ASSURED (ABRIL/2021)"));
-        t3.click();
-        WebElement t4  = driver.findElement(
-                        By.linkText("TESTE FUNCIONAL DE API SOAPUI (ABRIL/2021)"));
-        t4.click();
-        WebElement t5  = driver.findElement(
-                         By.linkText("TESTE FUNCIONAL DE API SWAGGER (ABRIL/2021)"));
-        t5.click();
-        WebElement t6  = driver.findElement(
-                         By.linkText("TESTE FUNCIONAL MOBILE (ABRIL/2021)"));
-        t6.click();
-        WebElement t7  = driver.findElement(
-                         By.linkText("TESTE FUNCIONAL WEB (ABRIL/2021)"));
-        t7.click();
-        WebElement t8  = driver.findElement(
-                         By.linkText("TESTE FUNCIONAl DE API REST ASSURED (ABRIL/2021)"));
-        t8.click();
-        WebElement t9  = driver.findElement(
-                         By.linkText("TESTES FUNCIONAIS WEB E MOBILE (ABRIL/2021)"));
-        t9.click();
-        WebElement t10  = driver.findElement(
-                         By.linkText("TESTES FUNCIONAIS WEB E MOBILE (JANEIRO/2021) "));
-        t10.click();
-
-
+            //testes funcionais
 
 
+
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
-
 }
